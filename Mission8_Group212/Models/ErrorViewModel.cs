8fb6ad9c45c488f330f5b5ada525b83a528cc699
dalschuetz <131074445+dalschuetz@users.noreply.github.com>
2025-02-26 08:31:46 -0700

    Add project files.

---
+++ Mission8_Group212/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Mission8_Group212.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
