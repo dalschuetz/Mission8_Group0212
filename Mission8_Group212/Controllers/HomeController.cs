b61a31126c68f729cd4b79b218d9c41ea9d4c410
dalschuetz <131074445+dalschuetz@users.noreply.github.com>
2025-02-27 15:26:33 -0700

    HomeControllerFirstSetup

--- Mission8_Group212/Controllers/HomeController.cs
+++ Mission8_Group212/Controllers/HomeController.cs
@@ -6,33 +6,100 @@ namespace Mission8_Group212.Controllers
 {
     public class HomeController : Controller
     {
-        private TaskModel _task;
+        private Mission8Context _context;
 
-        public HomeController(TaskModel instance)
+        public HomeController(Mission8Context instance)
         {
-            _task = instance;
+            _context = instance;
         }
         public IActionResult Index()
         {
             return View();
         }
 
+
+        //need to get correct information
         [HttpGet]
         public IActionResult Quadrants()
         {
-            return View();
-        }
-        [HttpPost]
-        public IActionResult Quadrants(int TaskId)
-        {
-            return View();
+            var list = _context.Tasks.ToList();
+
+            return View(list);
         }
 
+
+        //New
+        [HttpGet]
         public IActionResult TaskForm()
         {
-            return View();
+            ViewBag.Quadrants = _context.Quadrants.ToList();
+            ViewBag.Categories = _context.Categories.ToList();
+            return View(new TaskModel());
         }
 
+        [HttpPost]
+        public IActionResult TaskForm(TaskModel response)
+        {
+            if (ModelState.IsValid)
+            {
+                //saves form to database
+                _context.Tasks.Add(response);
+                _context.SaveChanges();
+
+                //reloads the form page to enter a new movie, no confirmation page
+                return RedirectToAction("Quadrants");
+            }
+            else
+            {
+                ViewBag.Quadrants = _context.Quadrants.ToList();
+                ViewBag.Categories = _context.Categories.ToList();
+
+                return View(response);
+            }
+        }
+
+
+        //Edit
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var editableTask = _context.Tasks
+                .Single(x => x.TaskId == id);
+
+            ViewBag.Quadrants = _context.Quadrants.ToList();
+            ViewBag.Categories = _context.Categories.ToList();
+
+            return View("TaskForm", editableTask);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(TaskModel updatedTask)
+        {
+            _context.Update(updatedTask);
+            _context.SaveChanges();
+
+            return RedirectToAction("Quadrants");
+        }
+
+
+        //Delete
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var deleteableTask = _context.Tasks
+                .Single(x => x.TaskId == id);
+
+            return View(deleteableTask);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(TaskModel deletedTask)
+        {
+            _context.Remove(deletedTask);
+            _context.SaveChanges();
+
+            return RedirectToAction("MovieList");
+        }
 
     }
 }
