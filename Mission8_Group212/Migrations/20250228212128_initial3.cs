8ad00b5f889c30b349f7372696a6b3db0b548d79
dalschuetz <131074445+dalschuetz@users.noreply.github.com>
2025-02-28 14:27:22 -0700

    Final Final Final Commit

---
+++ Mission8_Group212/Migrations/20250228212128_initial3.cs
@@ -0,0 +1,114 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Mission8_Group212.Migrations
+{
+    /// <inheritdoc />
+    public partial class initial3 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tasks_Categories_CategoryId",
+                table: "Tasks");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tasks_Quadrants_QuadId",
+                table: "Tasks");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Tasks_CategoryId",
+                table: "Tasks");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Tasks_QuadId",
+                table: "Tasks");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "DueDate",
+                table: "Tasks",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Completed",
+                table: "Tasks",
+                type: "INTEGER",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CategoryId",
+                table: "Tasks",
+                type: "INTEGER",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "DueDate",
+                table: "Tasks",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Completed",
+                table: "Tasks",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "INTEGER",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CategoryId",
+                table: "Tasks",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "INTEGER",
+                oldNullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tasks_CategoryId",
+                table: "Tasks",
+                column: "CategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tasks_QuadId",
+                table: "Tasks",
+                column: "QuadId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tasks_Categories_CategoryId",
+                table: "Tasks",
+                column: "CategoryId",
+                principalTable: "Categories",
+                principalColumn: "CategoryId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tasks_Quadrants_QuadId",
+                table: "Tasks",
+                column: "QuadId",
+                principalTable: "Quadrants",
+                principalColumn: "QuadId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
